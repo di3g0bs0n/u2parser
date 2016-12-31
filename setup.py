dab099a87e63eae9c6840ebfe5fe6b85105577e1
diego <diego>
2016-12-31 15:28:06 +0100

    v1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='u2parser',
-    version='1.1',
+    version='1.2',
     author='Diego Fernandez',
     author_email='di3g0bs0n@gmail.com',
     description=('A parser for Unified2 logs (Snort).'),
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/di3g0bs0n/u2parser',
     install_requires=['netaddr==0.7.18'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
         'Topic :: Utilities'
